b961807188cfa8fe93e6cceae95c5498e49888ac
airellzulkarnain <airell.zulkarnain@gmail.com>
2022-06-26 17:34:03 +0700

    resepsionis update status functionality finished

--- backend/app/routers/resepsionis.py
+++ backend/app/routers/resepsionis.py
@@ -12,5 +12,18 @@ def retrive_data(req: Request, cursor: MySQLConnection = Depends(MySQLConnection
 
 @router.put('/update_status/{id_reservasi}')
 def update_status(id_reservasi: int, cursor: MySQLConnection = Depends(MySQLConnection)):
-   pass
+   cursor.execute('''SELECT status.nama_status FROM status 
+                     WHERE status.id_status = (SELECT id_status 
+                     FROM reservasi WHERE id_reservasi = %s) LIMIT 1''', (id_reservasi, ))
+   status = cursor.fetchone()['nama_status']
+
+   if status == 'BOOKED' or status == 'CHECKED_IN':
+      cursor.execute('UPDATE reservasi SET id_status=((SELECT id_status FROM status WHERE nama_status = %s)+1) WHERE id_reservasi=%s', 
+      (status, id_reservasi))
+      cursor.commit()
+      cursor.close()
+      return {"message": "STATUS UPDATED", "last_status": status}
+
+   else: 
+      return {"message": "STATUS UPDATE FAILED", "last_status":status}
 
