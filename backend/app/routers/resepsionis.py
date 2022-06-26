55ade98440bae0e4d1a8e02fbe2fbf7aa140f41d
airellzulkarnain <airell.zulkarnain@gmail.com>
2022-06-26 17:14:28 +0700

    update Pipfile

--- backend/app/routers/resepsionis.py
+++ backend/app/routers/resepsionis.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Request
 from ..connection import MySQLConnection
 
 
 router = APIRouter(tags=['Resepsionis'], prefix='/resepsionis')
 
-@router.get('/')
-def root(sql: MySQLConnection = Depends(MySQLConnection)):
-   sql.execute('SELECT * FROM reservasi')
-   result = sql.fetchall()
-   sql.close()
-   return result
+
+@router.get('/reservasi')
+def retrive_data(req: Request, cursor: MySQLConnection = Depends(MySQLConnection)):
+   pass
+
+
+@router.put('/update_status/{id_reservasi}')
+def update_status(id_reservasi: int, cursor: MySQLConnection = Depends(MySQLConnection)):
+   pass
+
